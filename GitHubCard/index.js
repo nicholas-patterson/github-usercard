b7c779edb56b0df84614caf9e625853d5cbcca93
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-07-11 16:24:10 -0400

    finished one stretch goal chained promises

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,11 +4,61 @@
 */
 const me = "nicholas-patterson";
 const cards = document.querySelector(".cards");
-axios.get(`https://api.github.com/users/${me}`).then(data => {
-  console.log(data.data);
-  const element = createUserCard(data.data);
-  cards.appendChild(element);
-});
+axios
+  .get(`https://api.github.com/users/${me}`)
+  .then(data => {
+    console.log(data.data);
+    const element = createUserCard(data.data);
+    cards.appendChild(element);
+  })
+  .then(
+    axios.get("https://api.github.com/users/alasalle").then(data => {
+      const angelaData = createUserCard(data.data);
+      cards.appendChild(angelaData);
+    })
+  )
+  .then(
+    axios.get("https://api.github.com/users/brudnak").then(data => {
+      const brudnakData = createUserCard(data.data);
+      cards.appendChild(brudnakData);
+    })
+  )
+  .then(
+    axios.get("https://api.github.com/users/noahfranco").then(data => {
+      const noahfrancoData = createUserCard(data.data);
+      cards.appendChild(noahfrancoData);
+    })
+  )
+  .then(
+    axios.get("https://api.github.com/users/tetondan").then(data => {
+      const tetondanData = createUserCard(data.data);
+      cards.appendChild(tetondanData);
+    })
+  )
+  .then(
+    axios.get("https://api.github.com/users/dustinmyers").then(data => {
+      const dustinmyersData = createUserCard(data.data);
+      cards.appendChild(dustinmyersData);
+    })
+  )
+  .then(
+    axios.get("https://api.github.com/users/justsml").then(data => {
+      const justsmlData = createUserCard(data.data);
+      cards.appendChild(justsmlData);
+    })
+  )
+  .then(
+    axios.get("https://api.github.com/users/luishrd").then(data => {
+      const luishrdData = createUserCard(data.data);
+      cards.appendChild(luishrdData);
+    })
+  )
+  .then(
+    axios.get("https://api.github.com/users/bigknell").then(data => {
+      const bigknellData = createUserCard(data.data);
+      cards.appendChild(bigknellData);
+    })
+  );
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -30,24 +80,6 @@ axios.get(`https://api.github.com/users/${me}`).then(data => {
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [
-  "alasalle",
-  "brudnak",
-  "noahfranco",
-  "tetondan",
-  "dustinmuers",
-  "justsml",
-  "luishrd",
-  "bigknell"
-];
-
-followersArray.forEach(user => {
-  axios.get(`https://api.github.com/users/${user}`).then(data => {
-    const followerElement = createUserCard(data.data);
-    cards.appendChild(followerElement);
-  });
-});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
